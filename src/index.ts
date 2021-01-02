1894bcbd1eb5a270da2522e15fa05e1e96382ad2
Olexandr Pascal <futupas@gmail.com>
2021-01-02 21:28:18 +0200

    dist, static content

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 const app = express();
 const port = 8080; // default port to listen
 
+app.use(express.static('public'));
+
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
     res.send( "Hello world!" );
@@ -9,5 +11,5 @@ app.get( "/", ( req, res ) => {
 
 // start the Express server
 app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port }` );
+    console.log( `Server started at http://localhost:${ port }` );
 } );
